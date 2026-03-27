9814ec175149d9bd503a219d7264826c32b85beb
Thiago Gabriel <thiagogab.20@gmail.com>
2026-03-27 16:59:05 -0300

    feat: criação da auth

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from app.database import get_connection
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "API funcionando!"}
+
+@app.get("/test-db")
+def test_db():
+    try:
+        conn = get_connection()
+        conn.close()
+        return {"message": "Conexão com o banco de dados funcionando!"}
+    except Exception as e:
+        return {"error": str(e)}
