7c66211b0374c6ea610cb206cfff619d21f4c67f
Thiago Gabriel <thiagogab.20@gmail.com>
2026-04-01 23:35:11 -0300

    feat: adição da rota e da porta do backend

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import get_connection
 from app.routes import auth, prescriptions, doses, dashboard
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth.router, prefix="/auth", tags=["Autenticação"])
 app.include_router(prescriptions.router, prefix="/prescriptions", tags=["Prescrições"])
-app.include_router(dashboard.router, prefix="/dashboard", tags=["Dashboard"])
 app.include_router(doses.router, prefix="/doses", tags=["Doses"])
+app.include_router(dashboard.router, prefix="/dashboard", tags=["Dashboard"])
 
 @app.get("/")
 def root():
