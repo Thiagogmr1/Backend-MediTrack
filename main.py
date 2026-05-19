5dcd480eb33a8dfe266e6e10f973d9aaa563b460
Thiago Gabriel <thiagogab.20@gmail.com>
2026-05-19 02:20:40 -0300

    feat: teste com print

--- main.py
+++ main.py
@@ -13,11 +13,12 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 origins = os.getenv("ALLOWED_ORIGINS", "http://localhost:3000").split(",")
+print(f"ALLOWED_ORIGINS: {origins}")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
